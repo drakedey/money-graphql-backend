19388d643167ba87a05b8e513a72ea6754f6ea3b
Joskar Hernandez <joskar.hernandez@tuten.cl>
2020-02-25 21:11:25 -0500

    feature: unit tests setup

---
+++ src/tests/AutController.spec.ts
@@ -0,0 +1,22 @@
+import { createConnection } from 'typeorm';
+
+import LoginPayload from '../payload/LoginPayload';
+import AuthController from '../controller/AuthController';
+
+let authController: AuthController;
+
+beforeAll(async () => {
+  const conn = await createConnection();
+  authController = new AuthController(conn.manager);
+});
+
+
+describe('AuthCotroller', () => {
+  it('Should login a user and return token', async () => {
+    const loginData: LoginPayload = new LoginPayload();
+    loginData.email = 'joskarandres97@gmail.com';
+    loginData.password = '1234';
+    const token = await authController.login(loginData);
+    expect(token).toBeDefined();
+  });
+});
